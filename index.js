6b061be306908bbba7c75d0159686b802c398509
Wenbo Wang <jackie-1685@163.com>
2018-12-03 12:13:39 +0800

    add database and auto RESTful endpoint support

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+const Utils = require('util')
 const express = require('express')
 const bodyParser = require('body-parser')
-const Utils = require('util')
+const initlizeDatabase = require('./database')
 
 const app = express()
 app.use(bodyParser.json())
 
 const startServer = async () => {
+  await initlizeDatabase(app)
   const port = process.env.SERVER_PORT || 3000
   await Utils.promisify(app.listen).bind(app)(port)
   console.log(`Listening on port ${port}`)
